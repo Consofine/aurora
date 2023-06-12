28e476945fc883606b340f9646fe9bb4f2177374
Mike Considine <mike@koptional.com>
2023-06-11 20:43:19 -0500

    feat: add max tracker

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 import schedule
 from PIL import Image
 import pytesseract
+from datetime import datetime
 from twilio.rest import Client
 from dotenv import load_dotenv
 
@@ -12,6 +13,8 @@ load_dotenv()
 
 IMAGES_URL = "https://services.swpc.noaa.gov/products/animations/ovation_north_24h.json"
 BASE_URL = "https://services.swpc.noaa.gov"
+MIN_STRENGTH_THRESHOLD = 70.0
+RESET_TIME_HOURS = 12
 
 
 def try_parse_strength(strength):
@@ -60,21 +63,10 @@ def send_text(body, to_number=None):
     )
 
 
-def maybe_send_aurora_text(strength: float):
-    if strength > 70.0:
-        message = "Aurora is active! Predicted strength is {} GW".format(strength)
-        send_text(message)
-    return
-
-
-def check_aurora():
-    print("Checking aurora!")
-    try:
-        fetch_latest_image()
-        strength = try_read_aurora()
-        maybe_send_aurora_text(strength)
-    except:
-        pass
+def send_aurora_text(strength: float):
+    print("Sending aurora text for strength {}".format(strength))
+    message = "Aurora is active! Predicted strength is {} GW".format(strength)
+    send_text(message)
 
 
 def send_uptime_text():
@@ -82,8 +74,43 @@ def send_uptime_text():
     send_text("Aurora still running :)")
 
 
+class MaxKeeper:
+    max_strength = 0.0
+    max_set_at = None
+
+    def handle_strength_update(self, strength: float):
+        if strength > MIN_STRENGTH_THRESHOLD and strength > self.max_strength + 30:
+            self.max_strength = strength
+            self.max_set_at = datetime.now()
+            send_aurora_text(strength)
+
+    def check_should_clear_max(self):
+        if not self.max_set_at:
+            return False
+        diff = datetime.now() - self.max_set_at
+        timespan_in_seconds = 60 * 60 * RESET_TIME_HOURS
+        return diff.total_seconds() > timespan_in_seconds
+
+    def maybe_clear_max_strength(self):
+        if self.max_set_at and self.check_should_clear_max():
+            self.max_set_at = None
+            self.max_strength = 0.0
+
+
+def check_aurora(max_keeper: MaxKeeper):
+    print("Checking aurora!")
+    try:
+        fetch_latest_image()
+        strength = try_read_aurora()
+        max_keeper.maybe_clear_max_strength()
+        max_keeper.handle_strength_update(strength)
+    except:
+        pass
+
+
 if __name__ == "__main__":
-    schedule.every(5).minutes.do(check_aurora)
+    max_keeper = MaxKeeper()
+    schedule.every(5).minutes.do(check_aurora, max_keeper)
     schedule.every(1).day.do(send_uptime_text)
 
     while True:
